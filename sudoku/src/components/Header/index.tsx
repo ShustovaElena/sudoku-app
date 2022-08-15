c349e63668846ae7f8c30bf65877ba120f4014bc
Elena Shustova <alenyf-ka@mail.ru>
2022-08-15 11:41:51 +0300

    feat: add menu

--- sudoku/src/components/Header/index.tsx
+++ sudoku/src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { Menu } from '../Menu';
 import './index.css';
 
 export const Header = () => {
     return (
         <div className='header'>
-            <img className="logo" src='./favicon.png' alt='logo' />
-            <h1>Sudoku</h1>
+            <div className='logo'>
+                <img src='./favicon.png' alt='logo' />
+                <h1>Sudoku</h1>
+            </div>
+            <Menu />
         </div>
     );
 }
---
+++ sudoku/src/components/Menu/index.tsx
@@ -0,0 +1,10 @@
+import './index.css';
+
+export const Menu = () => {
+    return (
+        <nav className="menu">
+            <button className="button">Правила игры</button> 
+            <button className="button">Статистика</button>
+        </nav>
+    );
+};
