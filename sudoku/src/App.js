bb1a20829adeed85dd3634826377bc5a4711aa1a
Elena Shustova <alenyf-ka@mail.ru>
2022-08-16 09:24:11 +0300

    feat:  add screensaver

--- sudoku/src/App.js
+++ sudoku/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import { Main } from './pages/Main';
 import { Header } from './components/Header';
 
 function App() {
     return (
         <>
             <Header/>
+            <Main />
         </>
     );
 }
