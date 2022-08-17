94e3718ec7ce95a7687a40759c5a723d0738fb90
Elena Shustova <alenyf-ka@mail.ru>
2022-08-17 10:02:33 +0300

    feat: add rules page

--- sudoku/src/components/Header/index.tsx
+++ sudoku/src/components/Header/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { Menu } from '../Menu';
 import './index.css';
 
@@ -6,7 +7,9 @@ export const Header = () => {
     return (
         <div className='header'>
             <div className='logo'>
+                <Link to="/">
                 <img src='./favicon.png' alt='logo' />
+                </Link>
                 <h1>Sudoku</h1>
             </div>
             <Menu />
