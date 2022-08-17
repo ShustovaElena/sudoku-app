b4489f0484ebb4785e38b316e8bf4871671c6b95
Elena Shustova <alenyf-ka@mail.ru>
2022-08-17 10:26:50 +0300

    feat: add page not found

--- sudoku/src/components/AppRoutes/index.tsx
+++ sudoku/src/components/AppRoutes/index.tsx
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import { Main } from "../../pages/Main";
+import { NotFound } from "../../pages/NotFound";
 import { Rules } from "../../pages/Rules";
 
 export const AppRoutes = () => {
  return (
     <Routes>
       <Route path="/" element={<Main />} />
+      <Route path="*" element={<NotFound />} />
       <Route path="rules" element={<Rules />} />
       {/* <Route path="statistic" element={<Invoices />} /> */}
     </Routes>
